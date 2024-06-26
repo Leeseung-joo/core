4247c4a540f5ac74ba6250091924a6328dfd2cd1
Lee seungjoo <eun07213@gmail.com>
2024-06-26 20:32:01 +0900

    변경

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,11 +7,11 @@ import org.springframework.context.annotation.FilterType;
 
 import static org.springframework.context.annotation.ComponentScan.*;
 
-
-@Configuration
-@ComponentScan(
-        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
-
-public class AutoAppConfig{
-
-}
+//
+//@Configuration
+//@ComponentScan(
+//        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+//
+//public class AutoAppConfig{
+//
+//}
